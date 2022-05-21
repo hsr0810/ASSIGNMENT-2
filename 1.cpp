f195916374f521c18423f1fa94b2760d0a4bbda9
hsr0810 <105383900+hsr0810@users.noreply.github.com>
2022-05-21 10:38:48 +0530

    by Hrithik Singh

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	char ch ='a';
+	printf("alphabet from a-z are :/");
+	while(ch<='z')
+	{
+		printf("%c/n",ch);
+		ch++;
+	}
+}
---
+++ 3.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main ()
+{
+	int x= 23,y=56,product;
+	product=x*y;
+	printf("product of two number is %d"),product;
+}
